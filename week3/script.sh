b27a98b4837be589c852c4373357b2e4db87cae8
Pcheape <phillip.cheape@gmail.com>
2017-09-29 11:00:33 +0100

    changed to less than or equal also edited formatting

--- week3/script.sh
+++ week3/script.sh
@@ -2,7 +2,7 @@
 # this is a CPU counting script 
 CPUVAR=`grep 'processor' /proc/cpuinfo | wc -l`
 echo $CPUVAR
-if [[ $CPUVAR-lt2 ]];
-then echo $CPUVAR "less than 2 cant run update"
-exit 0 
+if [[ $CPUVAR -le 2 ]];
+ then echo $CPUVAR "less than 2 cant run update"
+ exit 0 
 fi
